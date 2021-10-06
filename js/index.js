5159bf714b9df29c9fd9d9800be89d1b8a506ec9
atoli07 <alexmaria_2001@hotmail.com>
2021-10-06 15:08:18 -0700

    Agregando nodemon

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 const express= require('express');
 const app= express();
 
+const morgan= require('morgan');
+
+app.set('port', 3500);
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.listen(3500, () => {
-    console.log('Server on port ${3500}');
+    console.log('Server on port '+app.get('port'));
 });
